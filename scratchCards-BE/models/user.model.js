0c8acd71d3f4d51ecf8b37c9aa0b3887ef8a80ba
rahul <devararahul56@gmail.com>
2025-10-06 13:40:53 -0500

    TASK-3::USER SCHEMA::created user schema and created post route to get user details also tested post route in postman

---
+++ scratchCards-BE/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from 'uuid';
+
+const userSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuidv4,
+    unique: true
+  },
+  userEmail: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt fields
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
--- scratchCards-BE/server.js
+++ scratchCards-BE/server.js
@@ -1,12 +1,30 @@
 import express from 'express';
 import { connectToDB } from './config/db.js';
-
-console.log('MongoDB URI:', process.env.MONGO_URI); // Log the MONGO_URI to verify it's loaded correctly
+import User from './models/user.model.js';
 
 const app = express();
 
+app.use(express.json()); // Middleware to parse JSON bodies
+
 app.get('/', (req, res) => {
   res.send('Hello World! welcome to Scratch Cards Backend');
+});
+
+app.post('/api/users', async (req, res) => {
+  const user = req.body // user will send this data
+
+  if (!user.firstName || !user.lastName || !user.userEmail) {
+    return res.status(400).json({ success: false, message: "firstName, lastName and userEmail are required fields" });
+  };
+
+  const newUser = new User(user);
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json({ success: true, data: savedUser });
+  } catch (error) {
+    console.error('Error saving user:', error);
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 app.listen(3000, () => {
