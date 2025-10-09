8c013955bdbe09db383426cdc57a10bf93a302d0
rahul <devararahul56@gmail.com>
2025-10-09 12:30:17 -0500

    TASK-9:: CORS CONFIGURATION :: added CORS middleware to server and updated package.json and package-lock.json to include cors dependency

--- scratchCards-BE/server.js
+++ scratchCards-BE/server.js
@@ -3,16 +3,26 @@ import { connectToDB } from './config/db.js';
 import userRoutes from './routes/user.route.js';
 import scratchCardRoutes from './routes/scratchCard.route.js';
 import transactionRoutes from './routes/transaction.route.js';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  })
+);
 
 app.use('/api/users', userRoutes);
 app.use('/api/scratchcards', scratchCardRoutes);
 app.use('/api/transactions', transactionRoutes);
 
 app.listen(3000, () => {
-  connectToDB(); // Connect to the database when the server starts
+  connectToDB();
   console.log('Server is running on port 3000...');
 });
